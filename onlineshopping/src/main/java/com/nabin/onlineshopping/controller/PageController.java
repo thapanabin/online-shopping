87a4354321074b42b2e80923d6909bc29cd42e8a
Nabin Thapa <thapanabincr7@outlook.com>
2019-06-27 09:32:42 -0500

    successfully Loaded static resources

---
+++ onlineshopping/src/main/java/com/nabin/onlineshopping/controller/PageController.java
@@ -0,0 +1,19 @@
+package com.nabin.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value = {"/", "/home","/index"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting","Welcome to the first Web Mvc Project.");
+		
+		return mv;
+	}
+	
+	
+}
